7ddc61575f3ba1d5c9fec8a4663215bb5905b38f
pm24mcgi <pmcginn@highlinefm.com>
2022-07-19 11:01:43 -0400

    heroku git commit -m heroku

--- config/database.js
+++ config/database.js
@@ -9,11 +9,10 @@ module.exports = {
     database,
     host,
     dialect: 'postgres',
-    seederStorage: 'sequelize',
   },
-  "production": {
-    "dialect": "postgres",
-    "seederStorage": "sequelize",
-    "use_env_variable": "DATABASE_URL"
+  production: {
+    use_env_variable: 'DATABASE_URL',
+    dialect: 'postgres',
+    seederStorage: 'sequelize',
   }
 };
